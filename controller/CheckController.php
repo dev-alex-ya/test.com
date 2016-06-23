fcc603d330737bc3159387fbf37c59e50926b66c
alex <ya.alexandr.86@gmail.com>
2016-06-23 21:47:55 +0300

    new check section, newOrder section

--- controller/CheckController.php
+++ controller/CheckController.php
@@ -7,26 +7,41 @@ $checks;
 
 if(!empty($_POST['total'])) //добавляем роль в БД
 {
-    $id_person = $_POST['id_person'];
-    
-    $time_out = Calendar::strToTimestamp($_POST['calendar']);
-    
-    
+    $got_on_hands = $_POST['got_on_hands'];
+    $blank_check = $_POST['blank_check'];
+    $parts_cost = $_POST['parts_cost'];
+    $transport = $_POST['transport'];
+    $managers_part = $_POST['managers_part'];
+    $masters_part = $_POST['masters_part'];
+    $commission = $_POST['commission'];
+    $total = $_POST['total'];
+    $debt = $_POST['debt'];
+    $transfer = $_POST['transfer'];
+    $date_transfer = Calendar::strToTimestamp($_POST['date_transfer']);
+        
     if(!empty($_POST['id']))
     {
-        //Check::updateCheck($id, $firstname, $middlename, $lastname, $id_role, $phone, $address, $card);
+//        Check::updateCheck($id, $got_on_hands, $blank_check, $parts_cost, 
+//                $transport, $managers_part, $masters_part, $commission, 
+//                $total,$debt,$transfer,$date_transfer);
     }
     else 
     {
-        Check::insertCheck($id_person, $time_in, $time_out, $title, $description);
+        Check::insertCheck($got_on_hands, $blank_check, $parts_cost,
+                $transport, $managers_part, $masters_part, $commission,
+                $total,$debt,$transfer,$date_transfer
+                );
     }    
     header('Refresh: 10; url=http://test.com/newCheck.php');
-    echo 'insert Check: '. "id=, id_person=$id_person, time_in=$time_in, time_out=$time_out, title=$title, description=$description";
+    echo 'insert Check: '. "id=, got_on_hands=$got_on_hands, blank_check=$blank_check, "
+            . "parts_cost=$parts_cost, transport=$transport, managers_part=$managers_part "
+            . "masters_part=$masters_part, commission=$commission, total=$total, "
+            . "debt=$debt, transfer=$transfer, date_transfer=$date_transfer";
 }
 if(!empty($_GET['id'])) //удаляем роль из БД
 {
     Check::removeById($_GET['id']);
-    header('Refresh: 0; url=http://test.com/');
+    header('Refresh: 0; url=http://test.com/newCheck.php');
     echo 'delete Check by id='.$_GET['id'];
 }
 else //отображаем все роли
--- model/Check.php
+++ model/Check.php
@@ -3,31 +3,35 @@ include_once 'config.php';
 class Check {
     static public function getAll()
     {
-        $query = "SELECT c.id, c.got_on_hands, c.blank_check, c.parts_cost, c.transport, c.managers_part, c.masters_part, c.commission, c.total, c.debt, c.transfer, c.date_transfer "
+        $query = "SELECT c.id, c.got_on_hands, c.blank_check, c.parts_cost, "
+                . "c.transport, c.managers_part, c.masters_part, c.commission, "
+                . "c.total, c.debt, c.transfer, c.date_transfer "
                 . "FROM b7_18317604_check AS c ";
-        $db = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);                
+        $db = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
         $db->query("SET NAMES 'utf8'");
         $result = $db->query($query) or die($db->error);
         $db->close();
         return $result->fetch_all();
     }
-    
-    static public function insertCheck($id_person, $time_in, $time_out, $title, $description)
+
+    static public function insertCheck($got_on_hands, $blank_check, $parts_cost,
+                $transport, $managers_part, $masters_part, $commission,
+                $total,$debt,$transfer,$date_transfer)
     {
         $link = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
         $now = time();
-        $query = "INSERT INTO b7_18317604_check () "
-                . "values()";
+        $query = "INSERT INTO b7_18317604_check (got_on_hands, blank_check, parts_cost, "
+               . "transport, managers_part, masters_part, commission, total, debt, transfer, date_transfer) "
+               . "values('$got_on_hands', '$blank_check', '$parts_cost', "
+               . "'$transport', '$managers_part', '$masters_part', '$commission', "
+               . "'$total', '$debt', '$transfer', '$date_transfer')";
         mysqli_query($link, "SET NAMES 'utf8'");
         mysqli_query($link, $query) or die(mysqli_error($link));
         mysqli_close($link);
     }
     
-    static public function updateCheck($id)
-    {
-        
-    }
-    
+    static public function updateCheck($id){}
+
     static public function removeById($id)
     {
         $query = "DELETE FROM b7_18317604_check WHERE id = $id ";
